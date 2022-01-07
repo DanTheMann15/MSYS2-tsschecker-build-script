7627b10e60f112a067d02d758e82c529c6be1205
Daniel <danthemann15@yahoo.com>
2022-01-07 10:42:24 -0500

    Update Scripts

--- build_tsschecker_win32.sh
+++ build_tsschecker_win32.sh
@@ -1,21 +1,31 @@
-echo -e "This script will help make a static build of tsschecker for Windows (i686)"
+echo -e "This script will create a static build of tsschecker for Windows 32bit (i686)"
 echo "INFO: Please make sure your package lists are updated before proceeding"
 echo "WARNING: Make sure you are using the MSYS2 MinGW 32-bit shell"
 read -p "Press ENTER to continue or hit CTRL+C to abort"
 echo -e "Preparing enviornment:"
 sleep 1
+echo -e "Setting PATH Environment variables:"
+export PATH=$PATH:/tmp/tsschecker_build_win32/bin
+export LIBRARY_PATH=$LIBRARY_PATH:/tmp/tsschecker_build_win32/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/tmp/tsschecker_build_win32/lib
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/tmp/tsschecker_build_win32/lib/pkgconfig:/tmp/tsschecker_build_win32/share/pkgconfig
+export C_INCLUDE_PATH=$C_INCLUDE_PATH:/tmp/tsschecker_build_win32/include
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/tmp/tsschecker_build_win32/include
+export OBJC_INCLUDE_PATH=$OBJC_INCLUDE_PATH:/tmp/tsschecker_build_win32/include
+sleep 1
+echo -e "Installing Required packages:"
 pacman -S --needed --noconfirm mingw-w64-i686-toolchain mingw-w64-i686-openssl mingw-w64-i686-libzip
 pacman -S --needed --noconfirm make automake autoconf autoconf-archive autogen bc bison flex cmake pkgconf openssl libtool m4 libidn2 git patch ed sed texinfo libunistring libunistring-devel python cython python-devel zsh
 echo "Packages installed, creating working directory:"
-export CURL_VERSION="7.80.0"
-export STATIC_FLAG="--enable-static --disable-shared"
+export CURL_VERSION="7.81.0"
+export BUILD_OPTIONS="--enable-static --disable-shared --prefix=/tmp/tsschecker_build_win32"
 export IS_STATIC=1
 mkdir ./tsschecker_build_win32
 cd ./tsschecker_build_win32
 set -e
 export CC=gcc
 export CXX=g++
-echo -e "Cloning required dependencies\n"
+echo -e "Cloning required dependencies:"
 sleep 1
 git clone --recursive https://github.com/libimobiledevice/libimobiledevice-glue.git
 git clone --recursive https://github.com/libimobiledevice/libplist.git
@@ -34,26 +44,26 @@ sleep 1
 echo "patching libfragmentzip"
 sed -i'' 's|fopen(savepath, \"w\")|fopen(savepath, \"wb\")|' ./libfragmentzip/libfragmentzip/libfragmentzip.c
 sleep 1
-echo "patches applied, continuing:\n"
+echo "patches applied, continuing: \n"
 sleep 1
 echo "Building curl"
 sleep 1
 wget https://curl.se/download/curl-$CURL_VERSION.tar.gz
 tar -zxvf curl-$CURL_VERSION.tar.gz
 cd ./curl-$CURL_VERSION
-./configure $STATIC_FLAG --disable-dependency-tracking --enable-ipv6 --with-winssl --with-schannel --with-winidn --without-ssl --with-zlib
+./configure $BUILD_OPTIONS --disable-dependency-tracking --enable-ipv6 --with-winssl --with-schannel --with-winidn --without-ssl --with-zlib
 make install
 cd ..
 echo "Building libplist"
 sleep 1
 cd ./libplist
-./autogen.sh $STATIC_FLAG --without-cython
+./autogen.sh $BUILD_OPTIONS --without-cython
 make install
 cd ..
 echo "Building libimobiledevice-glue"
 sleep 1
 cd ./libimobiledevice-glue
-./autogen.sh $STATIC_FLAG
+./autogen.sh $BUILD_OPTIONS
 make install
 cd ..
 echo "Building libirecovery"
@@ -64,25 +74,25 @@ aclocal -I m4
 autoheader
 automake --add-missing
 autoconf
-./configure --enable-static --disable-shared --with-dummy
+./configure $BUILD_OPTIONS --with-dummy --without-udev
 make install
 cd ..
 echo "Building libgeneral"
 sleep 1
 cd ./libgeneral
-./autogen.sh $STATIC_FLAG
+./autogen.sh $BUILD_OPTIONS
 make install
 cd ..
 echo "Building libfragmentzip"
 sleep 1
 cd ./libfragmentzip
-./autogen.sh $STATIC_FLAG
+./autogen.sh $BUILD_OPTIONS
 make install
 cd ..
 echo "Building tsschecker"
 sleep 1
 cd ./tsschecker
-./autogen.sh $STATIC_FLAG
+./autogen.sh $BUILD_OPTIONS
 make LDFLAGS=-all-static
 cd ..
 echo "Done building, cleaning up temp files:"
@@ -95,5 +105,9 @@ rm -fr libirecovery
 rm -fr libgeneral
 rm -fr libfragmentzip
 rm -f libirecovery.patch
+rm -fr /tmp/tsschecker_build_win32
+mv -f tsschecker/tsschecker/tsschecker.exe ../tsschecker.exe
+cd ..
+rm -fr tsschecker_build_win32
 echo "Done!"
 echo "tsschecker.exe can be found inside the working directory"
